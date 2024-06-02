42f740563b6e7cac826c8e0aeb10ad3979c4ad86
JMitac <mitacquispejuliocesar@gmail.com>
2024-06-02 13:08:49 -0500

    feat: adding dependencie vite-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/StackRickyAndMorty/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Stack Rick and Morty",
+        short_name: "Stack",
+        description: "App Rick and Morty",
+        theme_color: "#607D8B",
+        icons: [
+          {
+            src: "/assets/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/assets/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
+          },
+          {
+            src: "/assets/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
 })
