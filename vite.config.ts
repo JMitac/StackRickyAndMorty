4db482c175ca2c853f1da46cd713a430c8a1b315
JMitac <mitacquispejuliocesar@gmail.com>
2024-06-02 12:36:52 -0500

    feat: update url-base in viteConfig.js for deploy github-action

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/StackRickyAndMorty/",
+  base: "/StackRickyAndMorty/blob/gh-pages",
   plugins: [react()],
 })
